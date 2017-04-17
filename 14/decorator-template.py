033f47e927757efbcbfc9a5a6981e20761fb66f4
Lughead <jedmitten@alumni.cmu.edu>
2017-04-17 16:16:11 -0700

    write wrapper and return function if authorization key is not present

--- 14/decorator-template.py
+++ 14/decorator-template.py
@@ -1,8 +1,14 @@
 from functools import wraps
 
 
-def your_decorator(func):
-    '''Decorator to ...'''
+def not_authorized(func_name, *args, **kwargs):
+    print('You are not authorized for access to {} with args: {} and kwargs: {}'.format(func_name, args, kwargs))
+
+
+def requires_authkey(func):
+    """Decorator to check that a key is included in the kwargs as 'authkey'. If it is not present, a "not authorized"
+    function is returned.
+    """
     @wraps(func)  # preserves function meta data
     def wrapper(*args, **kwargs):
         # do stuff before func
