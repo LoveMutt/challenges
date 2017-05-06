9a8f47cb0c8a16b86d2e05544ae163892077f1d8
Lughead <jedmitten@alumni.cmu.edu>
2017-05-06 14:22:09 -0700

    calc highest value word in a list of words

--- challenge01/wordvalue.py
+++ challenge01/wordvalue.py
@@ -22,7 +22,16 @@ def calc_word_value(word):
 def max_word_value(word_list=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if word_list == None:
+        word_list = load_words()
+    max_val = -1
+    max_word = ''
+    for word in word_list:
+        word_val = calc_word_value(word)
+        if word_val > max_val:
+            max_val = word_val
+            max_word = word
+    return max_word
 
 if __name__ == "__main__":
     pass # run unittests to validate
