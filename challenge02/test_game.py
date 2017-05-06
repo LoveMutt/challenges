126e27301c20caab59ef1fb311a01ef73eea53cf
Lughead <jedmitten@alumni.cmu.edu>
2017-05-06 16:27:22 -0700

    update imports

--- challenge02/test_game.py
+++ challenge02/test_game.py
@@ -1,9 +1,9 @@
 import itertools
 import unittest
 
-from game import draw_letters, calc_word_value, max_word_value
-from game import get_possible_dict_words, _get_permutations_draw
-from game import _validation
+from challenge02.game import draw_letters, calc_word_value, max_word_value
+from challenge02.game import get_possible_dict_words, _get_permutations_draw
+from challenge02.game import _validation
 
 NUM_LETTERS = 7
 TEST_WORDS = ('bob', 'julian', 'pybites', 'quit', 'barbeque')
