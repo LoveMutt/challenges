761367ca1221cd20edf73f77526fec8e331536ec
Lughead <jedmitten@alumni.cmu.edu>
2017-05-06 14:19:42 -0700

    update file names and imports

---
+++ challenge01/wordvalue.py
@@ -0,0 +1,27 @@
+from challenge01.data import DICTIONARY, LETTER_SCORES
+
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    with open(DICTIONARY) as f:
+        words = f.read().split('\n')
+    return words
+
+def calc_word_value(word):
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    val = 0
+    for c in word:
+        for score, letters in LETTER_SCORES:
+            if c in letters:
+                val += 0
+                continue
+    return val
+
+def max_word_value():
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    pass
+
+if __name__ == "__main__":
+    pass # run unittests to validate
